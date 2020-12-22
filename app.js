ab8346c243164e3f445fdb47e57c259808cc0d11
Lily Lu <lilyzhaoyilu@gmail.com>
2020-12-21 20:02:50 -0800

    update

--- app.js
+++ app.js
@@ -1,52 +1,86 @@
+// const express = require("express");
+// const app = express();
+// const mongoose = require('mongoose');
+// const db = require('./config/keys').mongoURI;
+// const bodyParser = require('body-parser');
+// const users = require("./routes/api/users");
+// const tweets = require("./routes/api/tweets");
+
+// const passport = require('passport'); ///?
+
+// const User = require("./models/User")
+
+// mongoose
+// .connect(db, { useNewUrlParser: true })
+// .then(() => console.log("Connected to MongoDB successfully"))
+// .catch(err => console.log(err));
+
+// app.use(passport.initialize());
+// require('./config/passport')(passport);
+
+
+// app.use(bodyParser.urlencoded({
+//   extended: false
+// }))
+
+// app.use(bodyParser.json());
+
+
+
+// app.get("/", (req, res) => {
+//   const user = new User({
+//     handle: 'hh',
+//     email: 'jim@jj.com',
+//     password: '2sajkdhsajdkh'
+//   })
+//   user.save()
+  
+//   res.send("Helloc ddddddaaaaccca");
+  
+// });
+
+
+// app.use("/api/users", users);
+// app.use("/api/tweets", tweets);
+
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+
+
+// const port = process.env.PORT || 5000;
+
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
-
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 
-
-const User = require("./models/User")
-
 mongoose
-.connect(db, { useNewUrlParser: true })
-.then(() => console.log("Connected to MongoDB successfully"))
-.catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
+app.get("/", (req, res) => res.send("Hello World!!"));
 
-
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-
+/////////////////
+app.use(passport.initialize());
+require('./config/passport')(passport);
+/////////////////////////////
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
-
-app.get("/", (req, res) => {
-  const user = new User({
-    handle: 'hh',
-    email: 'jim@jj.com',
-    password: '2sajkdhsajdkh'
-  })
-  user.save()
-  
-  res.send("Helloc ddddddaaaaccca");
-  
-});
-
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-
-
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- config/passport.js
+++ config/passport.js
@@ -7,12 +7,55 @@ const keys = require('./keys')
 
 
 const options = {};
-options.jwtFormRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
-  passport.use(new JwtStrategy(options, (jwt_playload,done) => {
-    console.log(jwt_playload);
-    done();
+  passport.use(new JwtStrategy(options, (jwt_payload, done) => {
+    User.findById(jwt_payload.id)
+      .then(user => {
+        if (user) {
+          // return the user to the frontend
+          return done(null, user);
+        }
+        // return false since there is no user
+        return done(null, false);
+      })
+      .catch(err => console.log(err));
   }));
-}
+};
+
+
+// module.exports = passport => {
+//   passport.use(new JwtStrategy(options, function(jwt_payload, done) {
+//     User.findOne({id: jwt_payload.id}, function(err, user) {
+//         if (err) {
+//             return done(err, false);
+//         }
+//         if (user) {
+//             done(null, user);
+//         } else {
+//             done(null, false);
+//             // or you could create a new account
+//         }
+//     });
+// }))};
+
+
+
+// options.issuer = 'accounts.examplesoft.com';
+// options.audience = 'yoursite.net';
+// module.exports = passport => {
+//   passport.use(new JwtStrategy(options, function(jwt_payload, done) {
+//     User.findOne({id: jwt_payload.sub}, function(err, user) {
+//         if (err) {
+//             return done(err, false);
+//         }
+//         if (user) {
+//             return done(null, user);
+//         } else {
+//             return done(null, false);
+//             // or you could create a new account
+//         }
+//     });
+// }))};
