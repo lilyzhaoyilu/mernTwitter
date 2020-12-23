00e4bb26d8d0859fe20c38da887b4a736ee26e31
Lily Lu <lilyzhaoyilu@gmail.com>
2020-12-22 16:31:19 -0800

    Revert "Revert "update""
    This reverts commit 8c8a781708f1d09c916c53794c0b058002c24bac.

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+// keys_dev.js
+module.exports = {
+  mongoURI: 'mongodb+srv://lilylu:Lzy19930807@merntwitter.9ssaq.mongodb.net/MERNTwitter?retryWrites=true&w=majority',
+  secretOrKey: 'secret'
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload haha.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload haha.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// src/components/app.js
+
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
+import NavBarContainer from './nav/navbar_container';
+
+import TweetsContainer from './tweets/tweets_container';
+import MainPage from './main/main_page';
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
+import ProfileContainer from './profile/profile_container';
+import TweetComposeContainer from './tweets/tweet_compose_container';
+
+const App = () => (
+  <div>
+    <NavBarContainer />
+    <Switch>
+      <AuthRoute exact path="/" component={MainPage} />
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+
+      <ProtectedRoute exact path="/tweets" component={TweetsContainer} />
+      <ProtectedRoute exact path="/profile" component={ProfileContainer} />
+      <ProtectedRoute exact path="/new_tweet" component={TweetComposeContainer} />
+    </Switch>
+  </div>
+);
 
 export default App;
