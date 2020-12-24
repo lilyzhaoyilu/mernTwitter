f00257f5ae2b1e177d2cc79c348c35a38fd0fafb
Lily <lilyzhaoyilu@gmail.com>
2020-12-23 19:57:06 -0800

    changed build in app.js to public

--- app.js
+++ app.js
@@ -37,9 +37,9 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/source'));
+  app.use(express.static('frontend/public'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'source', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
   })
 }
 
