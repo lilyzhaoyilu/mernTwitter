cce94317fb6301e8426ff2165a547df1d5f5c039
Lily <lilyzhaoyilu@gmail.com>
2020-12-23 19:51:23 -0800

    local runs fine

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -10,54 +10,22 @@ import SignupFormContainer from './session/signup_form_container';
 import ProfileContainer from './profile/profile_container';
 import TweetComposeContainer from './tweets/tweet_compose_container';
 
-import Axios from "axios";
-
-class App extends React.Component {
-
-  constructor(props) {
-    super(props)
-  
-  }
- 
-
-  // callAPI() {
-  //   fetch("http://localhost:9000/")
-  //       .then(res => res.text())
-  //       .then(res => this.setState({ apiResponse: res }));  
-  // }
-
-  // componentWillMount() {
-  //   this.callAPI();
-  // }
-  render() {
-    Axios({
-      method: "GET",
-      url: "http://localhost:5000/",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }).then(res => {
-      console.log(res.data.message);
-    });
+// import Axios from "axios";
 
 
+const App = () => (
+  <div>
+    <NavBarContainer />
+    <Switch>
+      <AuthRoute exact path="/" component={MainPage} />
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
 
-
-    return (
-    <div>
-      <NavBarContainer />
-      <Switch>
-        <AuthRoute exact path="/" component={MainPage} />
-        <AuthRoute exact path="/login" component={LoginFormContainer} />
-        <AuthRoute exact path="/signup" component={SignupFormContainer} />
-
-        <ProtectedRoute exact path="/tweets" component={TweetsContainer} />
-        <ProtectedRoute exact path="/profile" component={ProfileContainer} />
-        <ProtectedRoute exact path="/new_tweet" component={TweetComposeContainer} />
-      </Switch>
-    </div>
-    )
-  }
-};
+      <ProtectedRoute exact path="/tweets" component={TweetsContainer} />
+      <ProtectedRoute exact path="/profile" component={ProfileContainer} />
+      <ProtectedRoute exact path="/new_tweet" component={TweetComposeContainer} />
+    </Switch>
+  </div>
+);
 
 export default App;
