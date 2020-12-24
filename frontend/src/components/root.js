7c0e7bb8acaa1fa33aacd5e50f204f6ccbbe8f16
Lily <lilyzhaoyilu@gmail.com>
2020-12-23 19:22:38 -0800

    edited app to jsx

--- frontend/src/components/root.js
+++ frontend/src/components/root.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
-import App from './app';
+import App from './app.jsx';
 
 
 const Root = ({ store }) => (
