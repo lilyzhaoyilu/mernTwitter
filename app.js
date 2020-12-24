c6e9f1b8778675a7995f976385eec25e2f28a756
Lily <lilyzhaoyilu@gmail.com>
2020-12-23 18:55:21 -0800

    this time??

--- app.js
+++ app.js
@@ -29,9 +29,12 @@ app.use(bodyParser.json());
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
-const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+
+
+const port = process.env.PORT || 5000;
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
@@ -39,3 +42,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
 }
+
+
+
+
